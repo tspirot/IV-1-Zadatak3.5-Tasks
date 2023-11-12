51be75932c3aeab2e38ce3b50e0332b6adc4dd44
Boban Blagojević <boban.blagojevic@tsp.edu.rs>
2023-11-13 00:22:47 +0100

    First commit

---
+++ Zadatak3.5/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Zadatak3._5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonSuspend = new System.Windows.Forms.Button();
+            this.buttonResume = new System.Windows.Forms.Button();
+            this.buttonAbort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(680, 33);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonSuspend
+            // 
+            this.buttonSuspend.Location = new System.Drawing.Point(680, 62);
+            this.buttonSuspend.Name = "buttonSuspend";
+            this.buttonSuspend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSuspend.TabIndex = 1;
+            this.buttonSuspend.Text = "Suspend";
+            this.buttonSuspend.UseVisualStyleBackColor = true;
+            this.buttonSuspend.Click += new System.EventHandler(this.buttonSuspend_Click);
+            // 
+            // buttonResume
+            // 
+            this.buttonResume.Location = new System.Drawing.Point(680, 91);
+            this.buttonResume.Name = "buttonResume";
+            this.buttonResume.Size = new System.Drawing.Size(75, 23);
+            this.buttonResume.TabIndex = 2;
+            this.buttonResume.Text = "Resume";
+            this.buttonResume.UseVisualStyleBackColor = true;
+            this.buttonResume.Click += new System.EventHandler(this.buttonResume_Click);
+            // 
+            // buttonAbort
+            // 
+            this.buttonAbort.Location = new System.Drawing.Point(680, 120);
+            this.buttonAbort.Name = "buttonAbort";
+            this.buttonAbort.Size = new System.Drawing.Size(75, 23);
+            this.buttonAbort.TabIndex = 3;
+            this.buttonAbort.Text = "Abort";
+            this.buttonAbort.UseVisualStyleBackColor = true;
+            this.buttonAbort.Click += new System.EventHandler(this.buttonAbort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonAbort);
+            this.Controls.Add(this.buttonResume);
+            this.Controls.Add(this.buttonSuspend);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonSuspend;
+        private System.Windows.Forms.Button buttonResume;
+        private System.Windows.Forms.Button buttonAbort;
+    }
+}
+
---
+++ Zadatak3.5/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadatak3._5
+{
+    public partial class Form1 : Form
+    {
+        bool stop = false;//animacija zaustavljena
+        int x1, y1, o1; // koordinate i dimenz. trougla
+        Color boja1; // boja trougla
+        Random r = new Random(); // gen sluc boje
+        Thread t1; // nit za metodu
+        public Form1()
+        {
+            InitializeComponent();
+            t1 = new Thread(new ThreadStart(Run1));
+            t1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            x1 = 0;
+            y1 = ClientRectangle.Height / 2;
+            o1 = 120;
+            //nijansa zelene boje - malo crvene i plave, puno zelene
+            boja1 = Color.FromArgb(r.Next(160), r.Next(170, 256), r.Next(160));
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            //if (!stop) return;
+            try
+            {
+                // pokretanje niti koristeci metodu Start()
+                t1 = new Thread(new ThreadStart(Run1));
+                t1.Start();
+                stop = false;
+            }
+            catch (ThreadStateException ex)
+            {
+                MessageBox.Show("Nit nije prekinuta");
+            }
+        }
+
+        private void buttonSuspend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // zaustavljanje iyvrsavanja niti koristeci metodu Suspend()
+                t1.Suspend();
+            }
+            catch (ThreadStateException ex)
+            {
+                MessageBox.Show("Nit nije pokrenuta");
+            }
+        }
+
+        private void buttonResume_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // nastavak izvrsavanja niti koristeci metodu Resume()
+                t1.Resume();
+            }
+            catch (ThreadStateException ex)
+            {
+                MessageBox.Show("Nit nije pauzirana");
+            }
+        }
+
+        private void buttonAbort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // zaustavljanje niti koristeci metodu Abort()
+                t1.Abort();
+                stop = true;
+            }
+            catch (ThreadStateException ex)
+            {
+                MessageBox.Show("Nit je zaustavljena");
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen olovka1 = new Pen(Color.Green, 5);
+            SolidBrush cetka1 = new SolidBrush(boja1);
+            Point p1 = new Point(x1, y1 - o1 / 2);
+            Point p2 = new Point(x1 + o1 / 2, y1);
+            Point p3 = new Point(x1, y1 + o1 / 2);
+            Point[] p = { p1, p2, p3 };
+            g.FillPolygon(cetka1, p);
+            g.DrawPolygon(olovka1, p);
+            base.OnPaint(e);
+        }
+        public void Run1()
+        {
+            x1 = 0;
+            while (!stop)
+            {
+                if (x1 < ClientRectangle.Width)
+                    x1 += 5;
+                else
+                {
+                    x1 = 0;
+                    boja1 = Color.FromArgb(r.Next(160), r.Next(170, 256), r.Next(160));
+                }
+                Invalidate(); // Poništava celu površinu kontrole i dovodi do ponovnog
+                Thread.Sleep(30);
+            }
+        }
+    }
+}
